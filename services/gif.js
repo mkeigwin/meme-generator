2f2f83242fd1da5cad75e648039deb3f0b5f4b1e
matt keigwin <mkeigwin@bates.edu>
2016-10-27 11:18:51 -0400

    tried lots of ways to get page to change...still no luck

--- services/gif.js
+++ services/gif.js
@@ -3,12 +3,13 @@ const API_URL = 'http://api.giphy.com/v1/gifs/search?';
 
 function searchGif (req, res, next) {
   const qs = req.body.search;
-  // let page = req.query.p;
- // fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=12&offset=${page}`)
+  let page = 0;
+  // let page = req.query.page = 0;
+  let number = req.query.number = 12;
+ fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=${number}&offset=${page}`)
 
-//make 5 pages, hide first 4 and hitting next hides one and makes another appear
 
-  fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=12&offset=0`)
+  // fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=12&offset=0`)
  .then(r => r.json())
  .then((result) => {
    res.gif = result.data;
--- services/meme.js
+++ services/meme.js
@@ -3,7 +3,11 @@ const API_URL = 'http://version1.api.memegenerator.net/Generators_Search?';
 
 function searchMeme (req, res, next) {
   const qs = req.body.search;
- fetch(`${API_URL}q=${qs}&pageIndex=0&pageSize=12`)
+  let page =  0;
+  // let page = req.query.page = 0;
+  let number = req.query.number = 12;
+  fetch(`${API_URL}q=${qs}&pageIndex=${page}&pageSize=${number}`)
+ // fetch(`${API_URL}q=${qs}&pageIndex=0&pageSize=12`)
  .then(r => r.json())
  .then((result) => {
    res.meme = result.result;
