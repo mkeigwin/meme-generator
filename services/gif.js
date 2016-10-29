ca823df68138208a7c7d58626420aca36f4fcd77
matt keigwin <mkeigwin@bates.edu>
2016-10-29 00:00:27 -0400

    search input setup

--- services/gif.js
+++ services/gif.js
@@ -4,7 +4,7 @@ const API_URL = 'http://api.giphy.com/v1/gifs/search?';
 
 function searchGif(req, res, next) {
   const qs = req.body.search || 'funny';
-  const page = (req.body.page + (req.body.number - 1)) || 0;
+  const page = (req.body.page + (req.body.number - 1)) || (0 + (req.body.number - 1));
   const number = req.body.number || 10;
 
   fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=${number}&offset=${page}`)
