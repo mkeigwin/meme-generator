abf7f5bfbf473debb9b3bc9988c8ed92e693b56d
matt keigwin <mkeigwin@bates.edu>
2016-10-30 16:51:20 -0400

    styled profile page

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
+$(document).ready(function () {
+  setTimeout(function () { $('.real').fadeIn(2000) }, 1200);
 
-$(document).ready(function() {
-
-setTimeout(function(){ $(".real").fadeIn()}, 2000);
+ $('#arrow').click(function () {
+      $('.alert').show();
+      $('.arrow2').show();
+      $('.arrow').hide();
+ });
 });
