8b1bf16c82d79c098813c90e5554e8224600daea
matt keigwin <mkeigwin@bates.edu>
2016-10-31 15:30:35 -0400

    comment models

--- models/favorites.js
+++ models/favorites.js
@@ -1,11 +1,17 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
+// I call the data base that I created generate, localhost 27017 comes from my mongod account
+// where my data will be stored
 const DB_CONNECTION = 'mongodb://localhost:27017/generate';
 
+// get favorites functions allows my library to retrieve data that was saved on the search page
 function getFavorites(req, res, next) {
   MongoClient.connect(DB_CONNECTION, (err, db) => {
     if (err) return next(err);
+// retrieve information from the collection favorites
     db.collection('favorites')
+// the session will depend on the user who is logged in
+// aka the library will only display results for that users favorites
       .find({ userId: { $eq: req.session.userId } })
       .toArray((toArrErr, data) => {
         if (toArrErr) return next(toArrErr);
@@ -18,25 +24,24 @@ function getFavorites(req, res, next) {
   return false;
 }
 
+// save favorite allows users to save and add images to their personal account based on Id
 function saveFavorite(req, res, next) {
-
-// this part below is attributed to trevor, My userId was not showing up so he "hacked" a way for it to appear
+// this part below is attributed to trevor, "hacked" a way for userId to appear
   const insertObj = {
     favorite: {
       userId: undefined,
     },
   };
-
-  // copying all of req.body into insertObj
+// copying all of req.body into insertObj. this part attributed to I-tunes
   for (key in req.body) {
     insertObj[key] = req.body[key];
   }
-
-  // Adding userId to insertObj
+// Adding userId to insertObj
   insertObj.favorite.userId = req.session.userId;
-  // console.log('INSERT OBJECT ****** ', insertObj.favorite);
+// console.log('INSERT OBJECT ****** ', insertObj.favorite);
   MongoClient.connect(DB_CONNECTION, (err, db) => {
     if (err) return next(err);
+// adding favorites to collection favorites based on userId
     db.collection('favorites')
       .insert(insertObj.favorite, (insertErr, result) => {
         if (insertErr) return next(insertErr);
@@ -49,12 +54,11 @@ function saveFavorite(req, res, next) {
   return false;
 }
 
-// Delete method doesn't change because we are deleting objects from the database
-// based on that object's unique _id - you do not need to specify which user as
-// the _id is sufficient enough
+// delete allows you to remove images from your stored database, as they appear in library
 function deleteFavorites(req, res, next) {
   MongoClient.connect(DB_CONNECTION, (err, db) => {
     if (err) return next(err);
+// removing from favorites
     db.collection('favorites')
       .findAndRemove({ _id: ObjectID(req.params.id) }, (removeErr, result) => {
         if (removeErr) return next(removeErr);
@@ -67,4 +71,5 @@ function deleteFavorites(req, res, next) {
   return false;
 }
 
+// exporting my three functions so that they can be called as middleware in my routes
 module.exports = { getFavorites, saveFavorite, deleteFavorites };
