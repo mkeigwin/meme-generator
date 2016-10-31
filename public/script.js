d8675915145ea04890d81316805970cc5f0c91a0
matt keigwin <mkeigwin@bates.edu>
2016-10-31 11:22:13 -0400

    links

--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
+
+console.log("linked");
+
   setTimeout(function () { $('.real').fadeIn(2000) }, 1200);
 
  $('#arrow').click(function () {
       $('.alert').show();
-      $('.arrow2').show();
-      $('.arrow').hide();
  });
 });
