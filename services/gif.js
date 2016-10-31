a9b598515e47ba10e20f25f48e5111cd5a84b486
matt keigwin <mkeigwin@bates.edu>
2016-10-31 16:46:46 -0400

    all js commented

--- services/gif.js
+++ services/gif.js
@@ -1,16 +1,23 @@
 const fetch = require('node-fetch');
+// used to fetch data from API
 
 const API_URL = 'http://api.giphy.com/v1/gifs/search?';
+// API info on gif come from
 
 function searchGif(req, res, next) {
   const qs = req.body.search || 'funny';
   const page = (req.body.page + (req.body.number - 1)) || (0 + (req.body.number - 1));
   const number = req.body.number || 10;
-
+// three search peramiters. make them equal qs page number so both api can work together
   fetch(`${API_URL}q=${qs}&api_key=dc6zaTOxFJmzC&limit=${number}&offset=${page}`)
+  // parameters are put in query string to render results based on searchs
+  // API key is still here!!! but it's ok haha
+  // Site I got it from made it publically availible, everyone uses the same key I guess
+  //Didn't have to sign up or anything to get the key, posted on site examples
   .then(r => r.json())
   .then((result) => {
     res.gif = result.data;
+// responce with results
     next();
   })
   .catch((err) => {
@@ -19,3 +26,4 @@ function searchGif(req, res, next) {
   });
 }
 module.exports = { searchGif };
+// export meme so it can be called as middleware
--- services/meme.js
+++ services/meme.js
@@ -1,15 +1,20 @@
 const fetch = require('node-fetch');
+// used to fetch data from API
 
 const API_URL = 'http://version1.api.memegenerator.net/Generators_Search?';
+// API info on meme come from
 
 function searchMeme(req, res, next) {
   const qs = req.body.search;
   const page = req.body.page || 0;
   const number = req.body.number || 10;
+// three search peramiters. make them equal qs page number so both api can work together
   fetch(`${API_URL}q=${qs}&pageIndex=${page}&pageSize=${number}`)
+// parameters are put in query string to render results based on searchs
   .then(r => r.json())
   .then((result) => {
     res.meme = result.result;
+// responce with results
     next();
   })
   .catch((err) => {
@@ -18,3 +23,4 @@ function searchMeme(req, res, next) {
   });
 }
 module.exports = { searchMeme };
+// export meme so it can be called as middleware
